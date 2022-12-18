408d5c734d2401364644727585fc50cd4ef1708a
iashishagarwal <2335037+iashishagarwal@users.noreply.github.com>
2022-12-17 20:10:22 -0800

    Update buddy.py
    version 2 
    Added support for styled text input and output for all print messages.

--- buddy.py
+++ buddy.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import argparse
+from colorama import Fore, Style
 
 # Create an argument parser
 parser = argparse.ArgumentParser()
@@ -13,24 +14,31 @@ parser.add_argument("-m", "--maxtokens", type=int, default=150, help="Maximum nu
 # Parse the arguments
 args = parser.parse_args()
 
+#Reset all styles
+print(Style.RESET_ALL)
+print(Style.DIM)
+
 # Check if debug mode is enabled
 if args.debug:
-  print("Debug mode is enabled.")
+  print(Fore.CYAN +"Debug mode is enabled.")
 else:
-  print("Debug mode is not enabled.")
+  print(Fore.WHITE +"Debug mode is not enabled.")
 
 # Print the temperature value
-print("Temperature:", args.temperature)
+print(Fore.WHITE +"Temperature:", args.temperature)
 
 # Print the maximum number of tokens
-print("Maximum number of tokens:", args.maxtokens)
+print(Fore.WHITE +"Maximum number of tokens:", args.maxtokens)
 
 #open api key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+#RESET THE STYLE
+print(Style.RESET_ALL)
+
 while True:
   # Take input from the user
-  s = input("Prompt: ")
+  s = input(Fore.MAGENTA + "> ")
 
   response = openai.Completion.create(
     model="text-davinci-003",
@@ -43,12 +51,14 @@ while True:
   )
 
   # Parse the response
-  response_text = response.choices[0].text
+  response_text = response.choices[0].text.strip()
 
   if args.debug:
+    print(Fore.CYAN)
+    print(Style.DIM)
     print(response)
+    print(Style.RESET_ALL)
 
   # Print the generated text
-  print(response_text)
-
-
+  print(Fore.YELLOW + response_text)
+  
